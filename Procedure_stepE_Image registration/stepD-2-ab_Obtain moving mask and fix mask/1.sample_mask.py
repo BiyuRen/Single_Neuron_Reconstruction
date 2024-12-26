b86ceae3a2ac40225de5c1ce0f530423b440b04e
xiaoyuuuuuu <byren@ion.ac.cn>
2024-12-26 14:43:39 +0800

    rename

--- Procedure_stepE_Image registration/stepD-2-ab_Obtain moving mask and fix mask/1.sample_mask.py
+++ Procedure_stepE_Image registration/stepD-2-ab_Obtain moving mask and fix mask/1.sample_mask.py
@@ -1,11 +1,10 @@
 # Author: SHIXIAOXUE
-# Purpose: merge 19 labeles from ccfv3 with different gray values
+# Purpose: merge 19 manually labeled brain images with different gray values
 import os
 import numpy as np
 import tifffile
-import nrrd
 import SimpleITK as sitk
-filepath=r'../../resource/19 labeles from ccfv3'
+filepath=r'../../resource/19 manually labeled brain images'
 output=r'../../resource/result'
 
 structureValue = [128,200,170,255,180,170,60,255,255,255,230,255,255,255,255,255,60,40,100]#gray values
@@ -13,15 +12,14 @@ count=0
 for regions in os.listdir(filepath):
     brainpath=os.path.join(filepath,regions)
     print(brainpath)
-    image, image_meta = nrrd.read(brainpath, index_order='F')
-    # tif = tifffile.imread(brainpath)
+    tif = tifffile.imread(brainpath)
     if count==0:
-        sample = np.zeros_like(image)
-    sample[image > 0] = structureValue[count]
+        sample = np.zeros_like(tif)
+    sample[tif > 0] = structureValue[count]
     count+=1
 x1 = sample[::2, ::2, ::2]# downsample to 20um
 
-new_tif_dir =os.path.join(output,'fix.tif')
+new_tif_dir =os.path.join(output,'sample.tif')
 tifffile.imwrite(new_tif_dir,x1)
 
 
--- Procedure_stepE_Image registration/stepD-2-ab_Obtain moving mask and fix mask/2.template_mask.py
+++ Procedure_stepE_Image registration/stepD-2-ab_Obtain moving mask and fix mask/2.template_mask.py
@@ -1,10 +1,11 @@
 # Author: SHIXIAOXUE
-# Purpose: merge 19 manually labeled brain images with different gray values
+# Purpose: merge 19 labeles from ccfv3 with different gray values
 import os
 import numpy as np
 import tifffile
+import nrrd
 import SimpleITK as sitk
-filepath=r'../../resource/19 manually labeled brain images'
+filepath=r'../../resource/19 labeles from ccfv3'
 output=r'../../resource/result'
 
 structureValue = [128,200,170,255,180,170,60,255,255,255,230,255,255,255,255,255,60,40,100]#gray values
@@ -12,14 +13,15 @@ count=0
 for regions in os.listdir(filepath):
     brainpath=os.path.join(filepath,regions)
     print(brainpath)
-    tif = tifffile.imread(brainpath)
+    image, image_meta = nrrd.read(brainpath, index_order='F')
+    # tif = tifffile.imread(brainpath)
     if count==0:
-        sample = np.zeros_like(tif)
-    sample[tif > 0] = structureValue[count]
+        sample = np.zeros_like(image)
+    sample[image > 0] = structureValue[count]
     count+=1
 x1 = sample[::2, ::2, ::2]# downsample to 20um
 
-new_tif_dir =os.path.join(output,'moving.tif')
+new_tif_dir =os.path.join(output,'template.tif')
 tifffile.imwrite(new_tif_dir,x1)
 
 
